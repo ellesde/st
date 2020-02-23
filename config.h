619b2f6b6f734e3572f6751a0d21448f4c09d591
ellesde <ellesde@protonmail.com>
2020-02-23 17:01:32 -0500

    Add padding

--- config.h
+++ config.h
@@ -5,9 +5,9 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "mono:pixelsize=24:antialias=true:autohint=true:dpi=166";
 static char *font2[] = { "JoyPixels:pixelsize=10:antialias=true:autohint=true" };
-static int borderpx = 2;
+static int borderpx = 40;
 
 /*
  * What program is execed by st depends of these precedence rules:
